759dfb97e1e677a7abfc58c77b62c28ff2c7594b
ZAHFOUF YOUNES <youneszahfouf@gmail.com>
2023-02-10 17:38:06 +0100

    some changes 2

--- CLIENT/src/components/Emp.vue
+++ CLIENT/src/components/Emp.vue
@@ -36,7 +36,32 @@ onMounted(()=>{fetch_user()})
              <a   class="btn btn-success" ref="ad" style="height: 40px;" href="http://127.0.0.1:8000/download" ><i style="    margin-right: 10px;" class="fa fa-download" aria-hidden="true"></i>Download</a>
         </div>
   <div class="row align-items-start">
+
+
+    <div v-if="user.length === 0" v-for="us in 3" class="col">
+
+      <div class="card" aria-hidden="true">
+        <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
+  <div class="card-body">
+    <h5 class="card-title placeholder-glow">
+      <span class="placeholder col-6"></span>
+    </h5>
+    <p class="card-text placeholder-glow">
+      <span class="placeholder col-4"></span>
+      <span class="placeholder col-6"></span>
+      <span class="placeholder col-7"></span>
+    </p>
+    <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
+  </div>
+</div>
+
+
+
+    </div>
+    
         <div v-for="us in  user" class="col"  >
+
+        
         
             <div class="card" style="width: 18rem;">
                 <RouterLink v-bind:to="'/employees/' + us.id" class="emp" >
@@ -65,7 +90,27 @@ onMounted(()=>{fetch_user()})
     
     }}</a></li>
   </ul>
+<!--
 
+    <div class="card" aria-hidden="true">
+        <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>
+  <div class="card-body">
+    <h5 class="card-title placeholder-glow">
+      <span class="placeholder col-6"></span>
+    </h5>
+    <p class="card-text placeholder-glow">
+      <span class="placeholder col-7"></span>
+      <span class="placeholder col-4"></span>
+      <span class="placeholder col-4"></span>
+      <span class="placeholder col-6"></span>
+      <span class="placeholder col-8"></span>
+    </p>
+    <a href="#" tabindex="-1" class="btn btn-primary disabled placeholder col-6"></a>
+  </div>
+</div>
+
+
+-->
 </nav>
 
 </template>
